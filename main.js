8f140cd9538033c689743e7e6e2311a7125ed181
Rafa Andrade <rafaelandradeslv@hotmail.com>
2021-10-31 00:20:33 -0300

    dicionário

--- main.js
+++ main.js
@@ -6,16 +6,43 @@ var idade = 28
 // alert('Bem vindo ' + nome);
 // alert(nome + ' tem ' + idade + ' anos')
 
-// ----- console.log() -----
-console.log(nome + ' tem ' + idade + ' anos')
+// // ----- console.log() -----
+// console.log(nome + ' tem ' + idade + ' anos')
 
-// ----- replace -----
-var frase = 'Japão é a melhor seleção que existe'
-console.log(frase.replace('Japão','Brasil'))
+// // ----- replace -----
+// var frase = 'Japão é a melhor seleção que existe'
+// console.log(frase.replace('Japão','Brasil'))
 
-// ----- toUpperCase() -----
-console.log(frase.toUpperCase())
+// // ----- toUpperCase() -----
+// console.log(frase.toUpperCase())
 
-// ----- toLowerCase() -----
-console.log(frase.toLowerCase())
+// // ----- toLowerCase() -----
+// console.log(frase.toLowerCase())
 
+// ----- lista -----
+// lista = ['Maçã','Pera','Uva','Banana']
+// console.log(lista)
+// // ----- acessando elemento -----
+// console.log(lista[1])
+// // --- adicionando item ---
+// lista.push('Laranja')
+// console.log(lista)
+// // --- removendo último item --- 
+// lista.pop()
+// console.log(lista) 
+// // --- tamanho da lista --- 
+// console.log(lista.length)
+// // --- revertendo ordem da lista
+// console.log(lista.reverse())
+// // --- transformando em string
+// console.log(lista.toString())
+// // --- join ---
+// console.log(lista.join(' - '))
+
+// --- dicionário --- 
+var fruta = {nome: 'maçã', cor:'vermelha'}
+console.log(fruta.nome)
+console.log(fruta.cor)
+// --- lista dicionário --- 
+var fruta = [{nome: 'laranja',cor:'laranja'}]
+console.log(fruta[0].cor)
