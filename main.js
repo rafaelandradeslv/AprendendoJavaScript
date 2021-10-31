e2b13925a4b21e5d1aa654e78a3a51f0206289f7
Rafa Andrade <rafaelandradeslv@hotmail.com>
2021-10-31 00:04:18 -0300

    trabalhando com strings

---
+++ main.js
@@ -0,0 +1,21 @@
+// ----- variáveis -----
+var nome = 'Rafael Andrade';
+var idade = 28
+
+// ----- alert -----
+// alert('Bem vindo ' + nome);
+// alert(nome + ' tem ' + idade + ' anos')
+
+// ----- console.log() -----
+console.log(nome + ' tem ' + idade + ' anos')
+
+// ----- replace -----
+var frase = 'Japão é a melhor seleção que existe'
+console.log(frase.replace('Japão','Brasil'))
+
+// ----- toUpperCase() -----
+console.log(frase.toUpperCase())
+
+// ----- toLowerCase() -----
+console.log(frase.toLowerCase())
+
